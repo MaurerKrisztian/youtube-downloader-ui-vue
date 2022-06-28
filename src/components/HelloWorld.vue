c9acc8dad500818a4d336ed381074ffd0234a637
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2022-06-28 21:54:59 +0200

    mvp

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+import socketClient from "socket.io-client";
+import ProgressBar from "./ProgressBar.vue";
+
+const SERVER = "http://localhost:3000";
+var socket = socketClient(SERVER);
+let input = ref("https://youtu.be/ouncVBiye_M")
+let progresses = ref<{ [pid: number]: { percent: number, totalSize: string, eta: string, currentSpeed: string, downloadLink?: string } }>({})
+
+const requestDownlaod = () => {
+  socket.emit('download-req', {link: input.value});
+}
+
+socket.on('download-progress', (data: {
+  meta: { pid: number }
+  percent: number
+  totalSize: string
+  eta: string,
+  currentSpeed: string
+}) => {
+  console.log(data)
+  progresses.value[data.meta.pid] = {...progresses.value[data.meta.pid], ...data}
+
+});
+
+
+socket.on('download-done', (data: { link: string, pid: number }) => {
+  console.log(data)
+  progresses.value[data.pid] = {...progresses.value[data.pid], downloadLink: data.link}
+});
+
+</script>
+
+<template>
+
+  <input type="text" v-model="input">
+  <button id="send" @click="requestDownlaod">send</button>
+  <div id="done"></div>
+
+  <div class="wrapper">
+    <template v-for="pid in Object.keys(progresses)" class="progress-container">
+
+      <div>{{ pid }}</div>
+      <div>
+        <ProgressBar v-bind:percent="progresses[pid].percent"></ProgressBar>
+      </div>
+      <div>{{ progresses[pid].currentSpeed }}</div>
+      <div>{{ progresses[pid].eta }}</div>
+      <div>{{ progresses[pid].totalSize }}</div>
+      <div><a v-bind:href="progresses[pid].downloadLink">Download</a></div>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: grid;
+  grid-template-columns: 15% 15% 15% 15% 15% 15%;
+}
+
+.wrapper {
+  background: #539ee3;
+  color: #0a0a0a;
+  border: 3px solid;
+  padding: 3px;
+  margin: 3px;
+}
+
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const props = defineProps(['percent'])
+</script>
+
+<template>
+  <div class="wrap-circles">
+    <div class="circle" :style=" {'background-image': 'conic-gradient(#B5838D '+props.percent+'%, #FFCDB2 0)' }">
+      <div class="inner"> {{props.percent}}%</div>
+    </div>
+  </div>
+</template>
+
+<style>
+
+body {
+  font-family: "Lato";
+  font-size: 16px;
+}
+
+.circle {
+  position: relative;
+  width: 150px;
+  height: 150px;
+  margin: 0.5rem;
+  border-radius: 50%;
+  background: #FFCDB2;
+  overflow: hidden;
+}
+.circle .inner {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 115px;
+  height: 115px;
+  background: #000;
+  border-radius: 50%;
+  font-size: 1.85em;
+  font-weight: 300;
+  color: rgba(255, 255, 255, 0.75);
+}
+</style>
